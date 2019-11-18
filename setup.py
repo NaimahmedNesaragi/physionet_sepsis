7ac5c0181339b13a788679d1aa0e9b0582de7072
Zachary Barnes <zsbarnes@knox.edu>
2019-11-18 10:56:09 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the onset of sepsis',
+    author='Zachary Barnes',
+    license='MIT',
+)
