a8265f34f62c0eb10eca36755723364733b005ca
Zachary Barnes <zsbarnes@knox.edu>
2020-01-08 09:47:50 -0800

    add notebooks

--- src/data/merge.sh
+++ src/data/merge.sh
@@ -1,2 +1,3 @@
 head -n 1 p000001.psv > merged.psv 
 for f in *.psv; do tail -n +2 -q "$f"; done >> merged.psv
+ls | sort -R | tail -6454 | xargs -I '{}' mv '{}' ~/physionet_sepsis/data/interim/validate/
